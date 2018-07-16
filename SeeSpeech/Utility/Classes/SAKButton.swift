190237f51658f28d9e9f4337f10e9d53cc2780d7
Susan Kern <skern@littlegreensoftware.com>
2018-07-16 13:12:46 -0400

    Commit CocoaPods

---
+++ SeeSpeech/Utility/Classes/SAKButton.swift
@@ -0,0 +1,48 @@
+//
+//  SAKButton.swift
+//
+//  Created by Susan Kern on 5/15/18.
+//  Copyright © 2018 SKern. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class SAKButton: UIButton {
+    
+    // MARK: IBInspectable variables
+   
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
---
+++ SeeSpeech/Utility/Classes/SAKView.swift
@@ -0,0 +1,47 @@
+//
+//  SAKView.swift
+//
+//  Created by Susan Kern on 5/15/18.
+//  Copyright © 2018 SKern. All rights reserved.
+//
+
+import UIKit
+
+class SAKView: UIView {
+    
+    // MARK: IBInspectable variables
+   
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+}
